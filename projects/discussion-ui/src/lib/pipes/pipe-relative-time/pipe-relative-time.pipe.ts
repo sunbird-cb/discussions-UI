244ca412d901013bdace5213339b84736ff5dc6b
bandana-sahu <vandana93sahu@gmail.com>
2021-03-31 17:22:14 +0530

    Issue #SB-23822 fix: Removed the day.js library

--- projects/discussion-ui/src/lib/pipes/pipe-relative-time/pipe-relative-time.pipe.ts
+++ projects/discussion-ui/src/lib/pipes/pipe-relative-time/pipe-relative-time.pipe.ts
@@ -1,19 +1,39 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+// import dayjs from 'dayjs';
+// import relativeTime from 'dayjs/plugin/relativeTime';
 
-dayjs.extend(relativeTime);
-
-
+// dayjs.extend(relativeTime);
 @Pipe({
   name: 'pipeRelativeTime',
 })
 export class PipeRelativeTimePipe implements PipeTransform {
-  transform(value: number): string {
+  transform(value: number): any {
+
     if (value) {
-      return dayjs((new Date(value))).fromNow();
+      const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+      if (seconds < 29) // less than 30 seconds ago will show as 'Just now'
+        return 'Just now';
+      const intervals = {
+        'year': 31536000,
+        'month': 2592000,
+        'week': 604800,
+        'day': 86400,
+        'hour': 3600,
+        'minute': 60,
+        'second': 1
+      };
+      let counter;
+      for (const i in intervals) {
+        counter = Math.floor(seconds / intervals[i]);
+        if (counter > 0)
+          if (counter === 1) {
+            return counter + ' ' + i + ' ago'; // singular (1 day ago)
+          } else {
+            return counter + ' ' + i + 's ago'; // plural (2 days ago)
+          }
+      }
     }
-    return dayjs().startOf('hour').fromNow();
+    return value;
   }
 }
 
