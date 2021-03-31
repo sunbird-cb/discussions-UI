38a83a1ce0ca13a5cfc9eaf657ab354bf7889392
bandana-sahu <vandana93sahu@gmail.com>
2021-03-31 18:00:41 +0530

    Issue #SB-23822 fix: removed the commented lines

--- projects/discussion-ui/src/lib/pipes/pipe-relative-time/pipe-relative-time.pipe.ts
+++ projects/discussion-ui/src/lib/pipes/pipe-relative-time/pipe-relative-time.pipe.ts
@@ -1,8 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-// import dayjs from 'dayjs';
-// import relativeTime from 'dayjs/plugin/relativeTime';
 
-// dayjs.extend(relativeTime);
 @Pipe({
   name: 'pipeRelativeTime',
 })
