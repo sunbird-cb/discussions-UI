e3a200071bc94ba2d959b0c80c7449765890151a
arunpilli21 <74179951+arunpilli21@users.noreply.github.com>
2021-04-24 23:32:26 +0530

    Issue #SB-21966 feat: WIdget Implementation

--- projects/discussion-ui/src/lib/wrapper-navigate.service.ts
+++ projects/discussion-ui/src/lib/wrapper-navigate.service.ts
@@ -5,6 +5,8 @@ import { EventsService } from './events.service';
 @Injectable({
   providedIn: 'root'
 })
+
+//TODO: Need to cleanup this service since we are not gonna use
 export class WrapperNavigateService {
 
   closeSideNav = new EventEmitter()
