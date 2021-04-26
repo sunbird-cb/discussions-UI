402deaa963ddc8ba2ab3ce226d099c6024fd2462
ArtiSingh23 <arti@zool.in>
2021-04-26 13:51:22 +0530

    Issue#SB-24299 Discussion forum >> alignment correction in modal and add rtl in css components

--- projects/discussion-ui/src/lib/elements/post-reply/post-reply.component.ts
+++ projects/discussion-ui/src/lib/elements/post-reply/post-reply.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'lib-post-reply',
   templateUrl: './post-reply.component.html',
-  styleUrls: ['./post-reply.component.css']
+  styleUrls: ['./post-reply.component.scss']
 })
 export class PostReplyComponent implements OnInit {
   @Input() showCancel = true;
