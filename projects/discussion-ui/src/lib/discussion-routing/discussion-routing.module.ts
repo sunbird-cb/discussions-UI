b239fda7fbd189b985b5be2764535ff231ab640b
bandana-sahu <vandana93sahu@gmail.com>
2021-04-26 14:55:24 +0530

    fix: Modified the constant.json

--- projects/discussion-ui/src/lib/discussion-routing/discussion-routing.module.ts
+++ projects/discussion-ui/src/lib/discussion-routing/discussion-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
       {
         path: '',
         pathMatch: 'full',
-        component: DiscussCategoryComponent
+        component: DiscussAllComponent
       },
       {
         path: 'all-discussions',
