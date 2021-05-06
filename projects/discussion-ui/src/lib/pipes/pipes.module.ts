bceacb8842b6bde5c798de43a922672a541843df
christopher.fernandes <christyabcd.05@gmail.com>
2021-05-06 20:22:43 +0530

    Issue #SB-24156 feat: Added search in tags page, added filter for it

--- projects/discussion-ui/src/lib/pipes/pipes.module.ts
+++ projects/discussion-ui/src/lib/pipes/pipes.module.ts
@@ -4,14 +4,15 @@ import { PipeFilterPipe } from './pipe-filter/pipe-filter.pipe';
 import { SortByPipe } from './sort-by/sort-by.pipe';
 import { PipeRelativeTimePipe } from './pipe-relative-time/pipe-relative-time.pipe';
 import { SplitInitialsPipe } from './split-initials/split-initials.pipe';
+import { PipeListFilterPipe } from './pipe-list-filter/pipe-list-filter.pipe';
 
 
 
 @NgModule({
-  declarations: [PipeFilterPipe, SortByPipe, PipeRelativeTimePipe, SplitInitialsPipe],
+  declarations: [PipeFilterPipe, SortByPipe, PipeRelativeTimePipe, SplitInitialsPipe, PipeListFilterPipe],
   imports: [
     CommonModule
   ],
-  exports: [PipeFilterPipe, SortByPipe, PipeRelativeTimePipe, SplitInitialsPipe]
+  exports: [PipeFilterPipe, SortByPipe, PipeRelativeTimePipe, SplitInitialsPipe, PipeListFilterPipe]
 })
 export class PipesModule { }
