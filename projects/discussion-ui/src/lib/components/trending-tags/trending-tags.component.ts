75c38fbc60bb66d9bc96cdffcb0945103e039cf7
christopher.fernandes <christyabcd.05@gmail.com>
2021-05-06 20:25:28 +0530

    Issue #SB-24156 fix: Style changes to match igot theming

--- projects/discussion-ui/src/lib/components/trending-tags/trending-tags.component.ts
+++ projects/discussion-ui/src/lib/components/trending-tags/trending-tags.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 import { NSDiscussData } from '../../models/discuss.model';
 /* tslint:disable */
 import _ from 'lodash'
+import { Router } from '@angular/router';
+import { ConfigService } from '../../services/config.service';
+import * as CONSTANTS from './../../common/constants.json';
 /* tslint:enable */
 @Component({
   selector: 'lib-discuss-trending-tags',
@@ -12,7 +15,11 @@ export class TrendingTagsComponent implements OnInit, OnChanges {
   @Input() tags!: NSDiscussData.ITag[];
   max = 0;
   trandingTags!: NSDiscussData.ITag[];
-  constructor() {
+  queryParam: any;
+  constructor(
+    public router: Router,
+    private configService: ConfigService,
+  ) {
 
   }
   ngOnInit(): void {
@@ -26,6 +33,19 @@ export class TrendingTagsComponent implements OnInit, OnChanges {
     this.max = _.get(_.maxBy(this.tags, 'score'), 'score') || 0;
     this.trandingTags = _.chain(this.tags).orderBy('score', 'desc').take(5).value();
   }
+
+  // TODO: To enable trending tags click and navigate to tags detals page
+  // getAllDiscussions(tag: { value: any }) {
+  //   this.queryParam = tag.value
+  //   const tagdata = {
+  //     tagname: ''
+  //   }
+  //   tagdata.tagname = tag.value
+  //   this.queryParam = tagdata
+
+  // tslint:disable-next-line: max-line-length
+  //   this.router.navigate([`${this.configService.getRouterSlug()}${CONSTANTS.ROUTES.TAG}tag-discussions`], { queryParams: this.queryParam });
+  // }
 
   css() {
     // return 'linear - gradient(to left, #00ff00 " + 80 + " %, #ff0000 20 %)"
