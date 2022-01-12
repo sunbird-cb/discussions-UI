78adee12455e9bc158a6a5f743ffd3cd833d6214
venkatkadiveti <venkatkadiveti248@gmail.com>
2022-01-12 08:52:48 +0530

    Issue #SB-28355 fix: post reply input boxes displaying multiple times and created topic not showing under my discussions

--- projects/discussion-ui/src/lib/components/discuss-start/discuss-start.component.ts
+++ projects/discussion-ui/src/lib/components/discuss-start/discuss-start.component.ts
@@ -117,7 +117,7 @@ export class DiscussStartComponent implements OnInit {
       });
     } else if (this.categoryId) {
       const req = {
-        cids: this.categoryId
+        cids: Array.isArray(this.categoryId) ? this.categoryId : [this.categoryId]
       };
       this.showSelectCategory = false;
       this.editable = false;
